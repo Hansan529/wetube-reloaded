4df0f73bd386aa813738d98e063c5ada66b8b8f6
hansan529 <hansan0529@gmail.com>
2023-01-30 23:44:01 +0900

    #4.0 What are Routers?

--- src/server.js
+++ src/server.js
@@ -5,19 +5,29 @@ const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
-
-const login = (req, res, next) => {
-  res.send(`${req.method} ${req.url}`);
-};
-
-const home = (req, res) => {
-  console.log("first");
-  return res.send("Home");
-};
-
 app.use(logger);
-app.get("/", home);
-app.get("/login", login);
+
+const globalRouter = express.Router();
+
+const handleHome = (req, res) => res.send("home");
+
+globalRouter.get("/", handleHome);
+
+const userRouter = express.Router();
+
+const handleEditUser = (req, res) => res.send("Edit User");
+
+userRouter.get("/edit", handleEditUser);
+
+const videoRouter = express.Router();
+
+const handleWatchVvideo = (req, res) => res.send("Watch  Video");
+
+videoRouter.get("/wtach", handleWatchVvideo);
+
+app.use("/", globalRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT} 🚀`);

cf724f4707d71deca90e72ab56e023f542623c40
hansan529 <hansan0529@gmail.com>
2023-04-10 17:06:33 +0900

    docs: 주소에 접속하게 되면 실행될 함수, 실행 될 함수 앞서 먼저 실행되는 use() 메소드

--- src/server.js
+++ src/server.js
@@ -4,8 +4,25 @@
 import express from "express";
 
 const PORT = 4000;
-
 const app = express();
+
+const gossipMiddleware = (req, res, next) => {
+  console.log(`누군가 ${req.url}로 이동하려고 합니다.`);
+  // console.log(req);
+  next();
+};
+
+const handleHome = (req, res) => {
+  console.log("메인 루트로 이동했습니다.");
+  return res.send("<h1>res send</h1>");
+};
+
+const handleLogin = (req, res) => {
+  return res.send("Login 완료");
+};
+app.use(gossipMiddleware);
+app.get("/", handleHome);
+app.get("/login", handleLogin);
 
 const handleListening = () => {
   console.log(`🐤 http://localhost:${PORT} 포트에서 listening 하고 있습니다.`);

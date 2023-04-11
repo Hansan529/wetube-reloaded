90b7c7e8cc034eb193d9cf8faa3d14c1a1f76c43
hansan529 <hansan0529@gmail.com>
2023-04-11 14:54:46 +0900

    refect: server.js -> controller, router 폴더로 분리

--- src/server.js
+++ src/server.js
@@ -3,21 +3,19 @@
 
 import express from "express";
 import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
 const PORT = 4000;
 
 const app = express(); // express 어플리케이션
 const logger = morgan("dev"); // morgan 미들웨어
 
-const handleHome = (req, res) => {
-  return res.send("<h1>res send</h1>");
-};
-
-const handleLogin = (req, res) => res.send("Login 완료");
-
 app.use(logger);
-app.get("/", handleHome);
-app.get("/login", handleLogin);
+app.use("/", globalRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
 
 const handleListening = () => {
   console.log(`🐤 http://localhost:${PORT} 포트에서 listening 하고 있습니다.`);

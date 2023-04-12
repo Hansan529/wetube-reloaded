33c1a65e43ff7bae30f9fc8b94a423bb9ac1ef9f
hansan529 <hansan0529@gmail.com>
2023-04-12 18:23:46 +0900

    modify: 지역배열->전역배열, see -> watch, req.parmas.id값 저장, 순서측정, 변수에 전송

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,33 +1,40 @@
+let videos = [
+  {
+    title: "첫번째 영상",
+    rating: 5,
+    comments: 2,
+    createdAt: "2분 전",
+    views: 59,
+    id: 1,
+  },
+  {
+    title: "두번째 영상",
+    rating: 2,
+    comments: 64,
+    createdAt: "59분 전",
+    views: 1532,
+    id: 2,
+  },
+  {
+    title: "세번째 영상",
+    rating: 6,
+    comments: 152,
+    createdAt: "1시간 전",
+    views: 665,
+    id: 3,
+  },
+];
+
 export const trending = (req, res) => {
-  const videos = [
-    {
-      title: "첫번째 영상",
-      rating: 5,
-      comments: 2,
-      createdAt: "2분 전",
-      views: 59,
-      id: 1,
-    },
-    {
-      title: "두번째 영상",
-      rating: 2,
-      comments: 64,
-      createdAt: "59분 전",
-      views: 1532,
-      id: 2,
-    },
-    {
-      title: "세번째 영상",
-      rating: 6,
-      comments: 152,
-      createdAt: "1시간 전",
-      views: 665,
-      id: 3,
-    },
-  ];
   return res.render("home", { pageTitle: "Home", videos });
 };
-export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
+export const watch = (req, res) => {
+  const {
+    params: { id },
+  } = req;
+  const video = videos[id - 1];
+  return res.render("watch", { pageTitle: `Watching ${video.title}`, video });
+};
 export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
 export const search = (req, res) => res.send("Search;");
 export const upload = (req, res) => res.send("Upload");

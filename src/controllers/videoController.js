9f85c7ec539f18294dbb2e968d3b459cced139d9
hansan529 <hansan0529@gmail.com>
2023-04-12 13:55:49 +0900

    docs: 배열 추가

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,10 +1,32 @@
-const fakeUser = {
-  username: "Hansan",
-  loggedIn: false,
+export const trending = (req, res) => {
+  const videos = [
+    {
+      title: "첫번째 영상",
+      rating: 5,
+      comments: 2,
+      createdAt: "2분 전",
+      view: 59,
+      id: 1,
+    },
+    {
+      title: "두번째 영상",
+      rating: 2,
+      comments: 64,
+      createdAt: "59분 전",
+      view: 1532,
+      id: 2,
+    },
+    {
+      title: "세번째 영상",
+      rating: 6,
+      comments: 152,
+      createdAt: "1시간 전",
+      view: 665,
+      id: 3,
+    },
+  ];
+  return res.render("home", { pageTitle: "Home", videos });
 };
-
-export const trending = (req, res) =>
-  res.render("home", { pageTitle: "Home", fakeUser });
 export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
 export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
 export const search = (req, res) => res.send("Search;");

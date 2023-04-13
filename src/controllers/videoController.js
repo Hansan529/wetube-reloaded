877ace99361dfeafd3beeee9a84b82c402d12c3a
hansan529 <hansan0529@gmail.com>
2023-04-13 12:37:34 +0900

    delete: 가짜 데이터베이스 삭제

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,53 +1,23 @@
-let videos = [
-  {
-    title: "첫번째 영상",
-    rating: 5,
-    comments: 2,
-    createdAt: "2분 전",
-    views: 59,
-    id: 1,
-  },
-  {
-    title: "두번째 영상",
-    rating: 2,
-    comments: 64,
-    createdAt: "59분 전",
-    views: 1532,
-    id: 2,
-  },
-  {
-    title: "세번째 영상",
-    rating: 6,
-    comments: 152,
-    createdAt: "1시간 전",
-    views: 665,
-    id: 3,
-  },
-];
-
 export const trending = (req, res) => {
-  return res.render("home", { pageTitle: "Home", videos });
+  return res.render("home", { pageTitle: "Home" });
 };
 export const watch = (req, res) => {
   const {
     params: { id },
   } = req;
-  const video = videos[id - 1];
-  return res.render("watch", { pageTitle: `Watching:  ${video.title}`, video });
+  return res.render("watch", { pageTitle: `Watching` });
 };
 export const getEdit = (req, res) => {
   const {
     params: { id },
   } = req;
-  const video = videos[id - 1];
-  res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
+  res.render("edit", { pageTitle: `Editing:` });
 };
 export const postEdit = (req, res) => {
   const {
     params: { id },
     body: { title },
   } = req;
-  videos[id - 1].title = title;
   return res.redirect(`/videos/${id}`);
 };
 export const getUpload = (req, res) => {
@@ -58,14 +28,5 @@ export const postUpload = (req, res) => {
   const {
     body: { title },
   } = req;
-  const newVideo = {
-    title,
-    rating: 0,
-    comments: 0,
-    createdAt: "방금",
-    views: 0,
-    id: videos.length + 1,
-  };
-  videos.push(newVideo);
   return res.redirect("/");
 };

fed9813b87100e8507a33d6a7afcbe106655e27f
hansan529 <hansan0529@gmail.com>
2023-04-13 12:35:49 +0900

    delete: init.js로 이동

--- src/server.js
+++ src/server.js
@@ -1,14 +1,11 @@
 // babel node를 사용하기 전에 express를 사용하는 법
 // const express = require("express");
 
-import "./db";
 import express from "express";
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
-
-const PORT = 4000;
 
 const app = express(); // express 어플리케이션
 const logger = morgan("dev"); // morgan 미들웨어
@@ -22,8 +19,4 @@ app.use("/", globalRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
 
-const handleListening = () => {
-  console.log(`🐤 http://localhost:${PORT} 포트에서 listening 하고 있습니다.`);
-};
-
-app.listen(PORT, handleListening);
+export default app;

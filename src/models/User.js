3b84dc5d6f5016e4b299a4d1615093908614de3e
Hansan529 <root@hxn.hyeon.pro>
2023-04-16 06:33:39 +0000

    docs: 최소 길이, 비밀번호 영문,특수문자 가능

--- src/models/User.js
+++ src/models/User.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  name: { type: String, required: true, unique: true, minLength: 2 },
+  username: { type: String, required: true, unique: true, minlength: 4 },
+  password: { type: String, required: true, minlength: 4 },
   email: { type: String, required: true, unique: true },
   emailType: { type: String, required: true },
   location: String,

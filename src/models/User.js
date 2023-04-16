808f72ffa73cbf1e4908e31c97c05ae08f7f5252
Hansan529 <root@hxn.hyeon.pro>
2023-04-16 06:14:55 +0000

    docs: 배열 위치 조정, 스키마 설정

--- src/models/User.js
+++ src/models/User.js
@@ -1,10 +1,11 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
+  name: { type: String, required: true, unique: true },
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  name: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  emailType: { type: String, required: true },
   location: String,
 });
 

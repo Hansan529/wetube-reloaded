ffba7a2a1aa4c4ecfdd5c6d2dc3794cceb84b759
Hansan529 <root@hxn.hyeon.pro>
2023-04-16 05:50:28 +0000

    docs: 포트 80-> 4000, 연결 완료 주소 추가

--- src/init.js
+++ src/init.js
@@ -3,10 +3,21 @@ import "./models/Video";
 import "./models/User";
 import app from "./server";
 
-const PORT = 80;
+const PORT = 4000;
 
 const handleListening = () => {
-  console.log(`🐤 http://localhost:${PORT} 포트에서 listening 하고 있습니다.`);
+  console.log(
+    `[localhost           ] 🐤 http://localhost:${PORT} 포트에서 listening 하고 있습니다.`
+  );
+  console.log(
+    `[hyeonServer - ip    ] 🐤 http://192.168.1.57:${PORT} 포트에서 listening 하고 있습니다.`
+  );
+  console.log(
+    `[hyeonServer - http  ] 🐤 http://hxan.net:${PORT} 포트에서 listening 하고 있습니다.`
+  );
+  console.log(
+    `[hyeonServer - https ] 🐤 https://hxan.net:${PORT} 포트에서 listening 하고 있습니다.`
+  );
 };
 
 app.listen(PORT, handleListening);

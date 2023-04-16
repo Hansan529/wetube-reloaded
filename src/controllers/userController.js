b1226906df29a5855c524c15201f5342b1d9c086
Hansan529 <root@hxn.hyeon.pro>
2023-04-16 06:16:56 +0000

    feat: 회원가입 데이터베이스 레이아웃 추가

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,26 @@
+import User from "../models/User";
+
 export const getJoin = (req, res) => {
-  console.log(req.body);
   return res.render("join", { pageTitle: "회원가입" });
+};
+export const postJoin = async (req, res) => {
+  const {
+    body: { name, username, password, email, emailType },
+  } = req;
+  console.log(emailType);
+  try {
+    await User.create({
+      name,
+      username,
+      password,
+      email,
+      required,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.render("404", { pageTitle: "오류가 발생했습니다" });
+  }
+  return res.redirect("/");
 };
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Delete User");

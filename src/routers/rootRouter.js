9bb386dbbf239ecc184a13716a47d349aaaaa07a
Hansan529 <root@hxn.hyeon.pro>
2023-04-16 06:16:19 +0000

    fix: 오타 수정

--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -1,11 +1,11 @@
 import express from "express";
 import { home, search } from "../controllers/videoController";
-import { getJoin, login } from "../controllers/userController";
+import { getJoin, postJoin, login } from "../controllers/userController";
 
 const rootRouter = express.Router();
 
 rootRouter.get("/", home);
-rootRouter.route("/join").get(getJoin), post(postJoin);
+rootRouter.route("/join").get(getJoin).post(postJoin);
 rootRouter.get("/login", login);
 rootRouter.get("/search", search);
 

d92289200be5bd282377997282b56219c29baf82
Hansan529 <hansan0529@gmail.com>
2023-04-17 03:06:04 +0000

    feat: 아이디 유무체크, 비밀번호 일치 체크
    findOnd으로 username 을 찾고, user가 없을 경우 상태코드 400의 오류 렌더링
    입력한 password와, username에 맞는 데이터베이스의 비밀번호와 대조

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,3 +1,4 @@
+import bcrypt from "bcrypt";
 import User from "../models/User";
 
 export const getJoin = (req, res) => {
@@ -32,16 +33,47 @@ export const postJoin = async (req, res) => {
       location,
     });
   } catch (error) {
-    console.log(error);
-    return res.render("join", {
+    return res.status(400).render("join", {
       pageTitle,
       errorMessage: error._message,
     });
   }
   return res.redirect("/");
 };
+
 export const edit = (req, res) => res.send("Edit User");
+
 export const remove = (req, res) => res.send("Delete User");
-export const login = (req, res) => res.send("Login");
+
+export const getLogin = (req, res) =>
+  res.render("login", { pageTitle: "로그인" });
+
+export const postLogin = async (req, res) => {
+  const pageTitle = "로그인";
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+
+  // 존재하지 않은 아이디를 입력했을 경우
+  if (!user) {
+    return res.status(400).render("login", {
+      pageTitle,
+      errorMessage: " 아이디 또는 비밀번호를 잘못 입력했습니다.",
+    });
+  }
+  const ok = await bcrypt.compare(password, user.password);
+  /* 비밀번호가 입력한 것과 일치하지 않을 경우 */
+  if (!ok) {
+    return res.status(400).render("login", {
+      pageTitle,
+      errorMessage: " 아이디 또는 비밀번호를 잘못 입력했습니다.",
+    });
+  }
+  return res.render("login", {
+    pageTitle,
+    errorMessage: "??",
+  });
+};
+
 export const logout = (req, res) => res.send("Logout");
+
 export const see = (req, res) => res.send("See User");

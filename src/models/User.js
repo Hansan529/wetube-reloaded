d442357881f23e9374541eaacccaaad5343f0e60
Hansan529 <hansan0529@gmail.com>
2023-04-17 00:13:17 +0000

    feat: pre를 통해, 'save'하기 전에 실행하는 middleware로, 해싱하도록 함
    bcrypt를 이용해서, this 즉, User.create 하는 요소의 password를 대입해서, 5번 해싱하도록 함

--- src/models/User.js
+++ src/models/User.js
@@ -1,3 +1,4 @@
+import bcrypt from "bcrypt";
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
@@ -5,8 +6,11 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true, minlength: 4 },
   password: { type: String, required: true, minlength: 4 },
   email: { type: String, required: true, unique: true },
-  emailType: { type: String, required: true },
   location: String,
+});
+
+userSchema.pre("save", async function () {
+  this.password = await bcrypt.hash(this.password, 5);
 });
 
 const User = mongoose.model("User", userSchema);

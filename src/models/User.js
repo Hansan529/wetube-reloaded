17da69dbaa856f605c5433bd2332d53dbbf99074
Hansan529 <hansan0529@gmail.com>
2023-04-19 03:05:24 +0000

    feat: 비밀번호 필수사항을 socialLogin의 값에 따라 true,false로 변경되도록, password가 없으면 해싱하지 않음
    기존 password는 required: true여서, 소셜로그인으로 생성할 때, 비밀번호를 생성하지 않아서 오류가 발생함
    해당 오류를 required에 function으로 해결함
    
    비밀번호가 없는 경우가 있으니, password가 있을 경우에만 해싱하도록 변경

--- src/models/User.js
+++ src/models/User.js
@@ -3,14 +3,24 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true, minLength: 2 },
+  socialLogin: { type: Boolean, default: false },
   username: { type: String, required: true, unique: true, minlength: 4 },
-  password: { type: String, required: true, minlength: 4 },
+  password: {
+    type: String,
+    required: function () {
+      return !this.socialLogin;
+    },
+    minlength: 4,
+  },
   email: { type: String, required: true, unique: true },
   location: { type: String, required: true },
 });
 
-userSchema.pre("save", async function () {
-  this.password = await bcrypt.hash(this.password, 5);
+userSchema.pre("save", async function (next) {
+  if (this.password) {
+    this.password = await bcrypt.hash(this.password, 5);
+  }
+  next();
 });
 
 const User = mongoose.model("User", userSchema);

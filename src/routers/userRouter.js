2e8cd6b39a3a40040c03d89e6ca5b15c0d2379ac
Hansan529 <hansan0529@gmail.com>
2023-04-22 11:37:36 +0000

    feat: 로그아웃, 프로필 수정에 대한 접근을 로그인한 유저에게만 적용

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -8,12 +8,13 @@ import {
   startGithubLogin,
   finishGithubLogin,
 } from "../controllers/userController";
+import { protectorMiddleware } from "../middlewares";
 
 const userRouter = express.Router();
 
-userRouter.route("/edit").get(getEdit).post(postEdit);
+userRouter.get("/logout", protectorMiddleware, logout);
+userRouter.route("/edit").all(protectorMiddleware).get(getEdit).post(postEdit);
 userRouter.get("/remove", remove);
-userRouter.get("/logout", logout);
 userRouter.get("/github/start", startGithubLogin);
 userRouter.get("/github/finish", finishGithubLogin);
 userRouter.get(":id", see);

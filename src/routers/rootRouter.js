9bbaa6e18ff44509951c2348633822a0da0d7fd7
Hansan529 <hansan0529@gmail.com>
2023-04-24 09:22:29 +0000

    update: 로그인,비로그인 보호 미들웨어 추가

--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -7,18 +7,18 @@ import {
   postLogin,
   logout,
 } from "../controllers/userController";
-import { publicOnlyMiddleware } from "../middlewares";
+import { protectorMiddleware, publicOnlyMiddleware } from "../middlewares";
 
 const rootRouter = express.Router();
 
 rootRouter.get("/", home);
-rootRouter.route("/join").get(getJoin).post(postJoin);
+rootRouter.route("/join").all(publicOnlyMiddleware).get(getJoin).post(postJoin);
 rootRouter
   .route("/login")
   .all(publicOnlyMiddleware)
   .get(getLogin)
   .post(postLogin);
-rootRouter.get("/logout", logout);
+rootRouter.get("/logout", protectorMiddleware, logout);
 rootRouter.get("/search", search);
 
 export default rootRouter;
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -8,15 +8,15 @@ import {
   startGithubLogin,
   finishGithubLogin,
 } from "../controllers/userController";
-import { protectorMiddleware } from "../middlewares";
+import { protectorMiddleware, publicOnlyMiddleware } from "../middlewares";
 
 const userRouter = express.Router();
 
 userRouter.get("/logout", protectorMiddleware, logout);
 userRouter.route("/edit").all(protectorMiddleware).get(getEdit).post(postEdit);
 userRouter.get("/remove", protectorMiddleware, remove);
-userRouter.get("/github/start", startGithubLogin);
-userRouter.get("/github/finish", finishGithubLogin);
+userRouter.get("/github/start", publicOnlyMiddleware, startGithubLogin);
+userRouter.get("/github/finish", publicOnlyMiddleware, finishGithubLogin);
 userRouter.get(":id", see);
 
 export default userRouter;

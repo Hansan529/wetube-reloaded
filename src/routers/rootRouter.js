8ed5fa529ac35b2e6beb9eb560b9213963b52950
Hansan529 <hansan0529@gmail.com>
2023-04-24 08:55:06 +0000

    update: 로그인 페이지 비로그인만 접근하도록 변경

--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -7,12 +7,17 @@ import {
   postLogin,
   logout,
 } from "../controllers/userController";
+import { publicOnlyMiddleware } from "../middlewares";
 
 const rootRouter = express.Router();
 
 rootRouter.get("/", home);
 rootRouter.route("/join").get(getJoin).post(postJoin);
-rootRouter.route("/login").get(getLogin).post(postLogin);
+rootRouter
+  .route("/login")
+  .all(publicOnlyMiddleware)
+  .get(getLogin)
+  .post(postLogin);
 rootRouter.get("/logout", logout);
 rootRouter.get("/search", search);
 

fb7cce6e1040b66e02e13ed46ef854df5b047d3f
Hansan529 <hansan0529@gmail.com>
2023-04-26 14:08:10 +0000

    docs: : 파라미터 사용

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -32,6 +32,6 @@ userRouter
 userRouter.get("/remove", protectorMiddleware, remove);
 userRouter.get("/github/start", publicOnlyMiddleware, startGithubLogin);
 userRouter.get("/github/finish", publicOnlyMiddleware, finishGithubLogin);
-userRouter.get(":id", see);
+userRouter.get("/:id", see);
 
 export default userRouter;

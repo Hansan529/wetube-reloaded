f5298da08630fa77468571722d479a04ed31b620
Hansan529 <hansan0529@gmail.com>
2023-04-28 01:09:45 +0000

    fix: 회원가입할 때와 비밀번호 변경시에만 해싱하도록 수정

--- src/models/User.js
+++ src/models/User.js
@@ -19,7 +19,9 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.pre("save", async function (next) {
-  if (this.password && this.isModfied("password")) {
+  if (this.isNew && this.password) {
+    this.password = await bcrypt.hash(this.password, 5);
+  } else if (this.isModified("password")) {
     this.password = await bcrypt.hash(this.password, 5);
   }
   next();

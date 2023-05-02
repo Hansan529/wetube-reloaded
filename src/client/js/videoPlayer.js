d36461555410c6456cf169567497162de50fb58e
Hansan529 <hansan0529@gmail.com>
2023-05-02 23:55:50 +0000

    update: 볼륨 실시간 업데이트, 시간 분리한 요소 선택

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,8 +1,12 @@
 const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
-const time = document.getElementById("time");
-const volume = document.getElementById("volume");
+const volumeRange = document.getElementById("volume");
+const currentTime = document.getElementById("currentTime");
+const totalTime = document.getElementById("totalTime");
+
+let volumeValue = 1;
+video.volume = volumeValue;
 
 const handlePlayClick = (e) => {
   if (video.paused) {
@@ -19,8 +23,27 @@ const handleMute = (e) => {
   } else {
     video.muted = true;
   }
-  muteBtn.innerText = video.muted ? "Mute" : "Unmute";
+  muteBtn.innerText = video.muted ? "Unmute" : "Mute";
+  volumeRange.value = video.muted ? 0 : volumeValue;
+};
+
+const handleVolumeChange = (event) => {
+  const {
+    target: { value },
+  } = event;
+  if (video.muted) {
+    video.muted = false;
+    muteBtn.innerText = "Mute";
+  }
+  volumeValue = value;
+  video.volume = value;
+};
+
+const handleLoadedMetadata = () => {
+  totalTime.innerText = Math.floor(video.duration);
 };
 
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMute);
+volumeRange.addEventListener("input", handleVolumeChange);
+video.addEventListener("loadedmetadata", handleLoadedMetadata);

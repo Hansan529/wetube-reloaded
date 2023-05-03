5b00a6b1065c16c17691608bd19520ad70ba1b40
Hansan529 <hansan0529@gmail.com>
2023-05-03 06:13:41 +0000

    update: views 추가

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -145,3 +145,16 @@ export const search = async (req, res) => {
   }
   return res.render("videos/search", { pageTitle, videos });
 };
+
+export const registerView = async (req, res) => {
+  const {
+    params: { id },
+  } = req;
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.status(404);
+  }
+  video.meta.views = video.meta.views + 1;
+  await video.save();
+  return res.status(200);
+};

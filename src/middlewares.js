f6af7b9b558fb426016561413e3811f90d6413d7
Hansan529 <hansan0529@gmail.com>
2023-05-04 01:27:05 +0000

    update: 오류 메시지 flash 설정

--- src/middlewares.js
+++ src/middlewares.js
@@ -12,6 +12,7 @@ export const protectorMiddleware = (req, res, next) => {
   if (req.session.loggedIn) {
     return next();
   } else {
+    req.flash("error", "승인되지 않은 접근입니다.");
     return res.redirect("/login");
   }
 };
@@ -20,6 +21,7 @@ export const publicOnlyMiddleware = (req, res, next) => {
   if (!req.session.loggedIn) {
     return next();
   } else {
+    req.flash("error", "승인되지 않은 접근");
     return res.redirect("/");
   }
 };

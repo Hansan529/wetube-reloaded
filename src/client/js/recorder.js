71b59448b21e29a71ad2d86a83991420c44b0e9b
Hansan529 <hansan0529@gmail.com>
2023-05-07 02:03:58 +0000

    update: 웹캠 프리뷰

---
+++ src/client/js/recorder.js
@@ -0,0 +1,13 @@
+const startBtn = document.getElementById("startBtn");
+const video = document.getElementById("preview");
+
+const handleStart = async () => {
+  const stream = await navigator.mediaDevices.getUserMedia({
+    audio: false,
+    video: { width: 200, height: 200 },
+  });
+  video.srcObject = stream;
+  video.play();
+};
+
+startBtn.addEventListener("click", handleStart);

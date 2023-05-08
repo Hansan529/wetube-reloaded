14b91bd8e8dc5926a31b8ae50b9208d06a0f6e18
Hansan529 <hansan0529@gmail.com>
2023-05-08 06:02:15 +0000

    update: 외부 참조 링크 허용

--- src/server.js
+++ src/server.js
@@ -30,6 +30,11 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  res.header("Cross-Origin-Embedder-Policy", "require-corp");
+  res.header("Cross-Origin-Opener-Policy", "same-origin");
+  next();
+});
 app.use(flash());
 app.use(localsMiddleware);
 app.use("/uploads", express.static("uploads"));

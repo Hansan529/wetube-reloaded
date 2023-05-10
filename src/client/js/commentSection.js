97e2cfcad1c2d077ad59b778bce722238748e581
Hansan529 <hansan0529@gmail.com>
2023-05-10 03:14:43 +0000

    update: api POST 형식으로 호출

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,11 +1,17 @@
 const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
 
-const handleSubmit = (e) => {
+const handleSubmit = async (e) => {
   e.preventDefault();
   const textarea = form.querySelector("textarea");
   const text = textarea.value;
-  const video = videoContainer.dataset.id;
+  const videoId = videoContainer.dataset.id;
+  await fetch(`/api/videos/${videoId}/comment`, {
+    method: "POST",
+    body: {
+      text,
+    },
+  });
 };
 
 if (form) {

05b76e99523824dc5c04c7ca32d18956a86223eb
Hansan529 <hansan0529@gmail.com>
2023-05-11 02:18:19 +0000

    update: 자바스크립트로 실시간 댓글처럼 보이도록 하는 방법

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,15 +1,38 @@
 const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
+const videoId = videoContainer.dataset.id;
+
+const addComment = async (text) => {
+  const videoComments = document.querySelector(".video__comments ul");
+  const newComment = document.createElement("li");
+  newComment.className = "video__comment";
+
+  const img = document.createElement("img");
+  img.className = "video__comment-owner";
+  const response = await fetch(`/api/videos/${videoId}/profile`, {
+    method: "POST",
+  });
+  const data = await response.json();
+  const avatarUrl = data.avatarUrls[data.avatarUrls.length - 1];
+  img.src = avatarUrl;
+  img.crossOrigin = "anonymous";
+
+  const span = document.createElement("span");
+  span.innerText = text;
+
+  newComment.appendChild(img);
+  newComment.appendChild(span);
+  videoComments.prepend(newComment);
+};
 
 const handleSubmit = async (e) => {
   e.preventDefault();
   const textarea = form.querySelector("textarea");
   const text = textarea.value;
-  const videoId = videoContainer.dataset.id;
   if (text.trim() === "") {
     return;
   }
-  await fetch(`/api/videos/${videoId}/comment`, {
+  const { status } = await fetch(`/api/videos/${videoId}/comment`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -17,9 +40,14 @@ const handleSubmit = async (e) => {
     body: JSON.stringify({ text }),
     credentials: "same-origin",
   });
+  if (status === 201) {
+    addComment(text);
+  }
   textarea.value = "";
 };
 
 if (form) {
   form.addEventListener("submit", handleSubmit);
 }
+
+// TODO: 코멘트 삭제

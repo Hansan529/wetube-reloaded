dbcb23a60cf694873ebd622f60f639a6fc33f26c
Hansan529 <hansan0529@gmail.com>
2023-05-11 02:19:15 +0000

    update: 이미지 fetch할 router

--- src/routers/apiRouter.js
+++ src/routers/apiRouter.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { createComment, registerView } from "../controllers/videoController";
+import {
+  commentProfile,
+  createComment,
+  registerView,
+} from "../controllers/videoController";
 
 const apiRouter = express.Router();
 
 apiRouter.post("/videos/:id([0-9a-f]{24})/view", registerView);
 apiRouter.post("/videos/:id([0-9a-f]{24})/comment", createComment);
+apiRouter.post("/videos/:id([0-9a-f]{24})/profile", commentProfile);
 
 export default apiRouter;

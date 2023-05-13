96c0e44b98d1425c93450f2b37cfd6889bff7f4e
Hansan529 <hansan0529@gmail.com>
2023-05-13 04:14:35 +0000

    fix: 정규표현식 범위 미표기 수정

--- src/routers/apiRouter.js
+++ src/routers/apiRouter.js
@@ -10,7 +10,7 @@ const apiRouter = express.Router();
 
 apiRouter.post("/videos/:id([0-9a-f]{24})/view", registerView);
 apiRouter.post("/videos/:id([0-9a-f]{24})/comment", createComment);
-apiRouter.post("/videos/:id[0-9a-f]{24}/comment-delete", deleteComment);
+apiRouter.post("/videos/:id([0-9a-f]{24})/comment-delete", deleteComment);
 apiRouter.post("/videos/:id([0-9a-f]{24})/profile", commentProfile);
 
 export default apiRouter;

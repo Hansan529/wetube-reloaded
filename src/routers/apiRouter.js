94d864bf2c3992a3c878a7a90790a2531826f72f
Hansan529 <hansan0529@gmail.com>
2023-05-13 02:28:28 +0000

    update: 댓글 삭제 API 라우터 지정

--- src/routers/apiRouter.js
+++ src/routers/apiRouter.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
   commentProfile,
   createComment,
+  deleteComment,
   registerView,
 } from "../controllers/videoController";
 
@@ -9,6 +10,7 @@ const apiRouter = express.Router();
 
 apiRouter.post("/videos/:id([0-9a-f]{24})/view", registerView);
 apiRouter.post("/videos/:id([0-9a-f]{24})/comment", createComment);
+apiRouter.post("/videos/:id[0-9a-f]{24}/comment-delete", deleteComment);
 apiRouter.post("/videos/:id([0-9a-f]{24})/profile", commentProfile);
 
 export default apiRouter;
